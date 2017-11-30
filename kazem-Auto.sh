8b820b81c7114da3c9c04612a5aea6b4dc66bc1a
kazem199700 <34087957+kazem199700@users.noreply.github.com>
2017-12-01 02:13:14 +0300

    Delete kazem-Auto.sh

--- kazem-Auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1
-while(true) do
-./kazem.sh
-curl "https://api.telegram.org/bot[362136053:AAGkJVHSOaAHFADlrQmKDn28AGLDNIbEA5c]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#kazem-Reloaded-${COUNTER}-times"
-let COUNTER=COUNTER+1 
-done
